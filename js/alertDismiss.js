e7f9dbfb23c7023eff1500567e3794b0f514d395
natinho68 <mandemons1@hotmail.com>
2017-06-20 17:49:38 +0200

    minor refactoring

--- js/alertDismiss.js
+++ js/alertDismiss.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
            window.setTimeout(function() {
            $(".alert").fadeTo(500, 0).slideUp(500, function(){
            $(this).remove();
